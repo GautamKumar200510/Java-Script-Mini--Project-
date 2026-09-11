ca33e3bc4c8bc263dd9d31584959e1a7d1b15063
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-09-11 18:00:13 +0530

    Reset Game

--- src/Game/simon.js
+++ src/Game/simon.js
@@ -8,88 +8,155 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function () {
+
+// START GAME
+document.addEventListener("keydown", function () {
+
     if (started === false) {
-        console.log("game is start");
+
+        console.log("game is started");
+
         started = true;
 
         levelup();
     }
+
 });
 
 
+// GAME FLASH
 function Flash(btn) {
+
     btn.classList.add("flash");
 
     setTimeout(function () {
+
         btn.classList.remove("flash");
+
     }, 250);
+
 }
 
+
+// USER FLASH
 function userFlash(btn) {
+
     btn.classList.add("userflash");
 
-    setTimeout(function (){
+    setTimeout(function () {
+
         btn.classList.remove("userflash");
+
     }, 250);
+
 }
 
+
+// LEVEL UP
 function levelup() {
+
     userSeq = [];
+
     level++;
+
     h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random() * 3);
+
+    // RANDOM BUTTON
+    let randIdx = Math.floor(Math.random() * 4);
+
     let randcolor = btns[randIdx];
 
     let randBtn = document.querySelector(`.${randcolor}`);
 
+
+    // ADD COLOR TO GAME SEQUENCE
     gameSeq.push(randcolor);
 
     console.log(gameSeq);
 
-    gameFlash(randBtn);   
+
+    // FLASH RANDOM BUTTON
+    Flash(randBtn);
+
 }
 
-function checkAns(idx){
-    // let idx = userSeq.length - 1
 
-    if (userSeq[idx] == gameSeq[idx]) {
-     if(userSeq.length == gameSeq.length){
-        setTimeout(levelup,1000);
-     }
-    }else {
-        h2.innerText = 'Game Over! Press any key to start';
-        reset();
+// CHECK ANSWER
+function checkAns(idx) {
+
+    // CORRECT ANSWER
+    if (userSeq[idx] === gameSeq[idx]) {
+
+        // COMPLETE SEQUENCE
+        if (userSeq.length === gameSeq.length) {
+
+            setTimeout(function () {
+
+                levelup();
+
+            }, 1000);
+
+        }
+
     }
+
+    // WRONG ANSWER
+    else {
+
+        h2.innerText = "Game Over! Press any key to start";
+
+        reset();
+
+    }
+
 }
 
+
+// BUTTON PRESS
 function btnPress() {
-    console.log(this);
+
+    // GAME START HONE SE PEHLE BUTTON CLICK NAHI HOGA
+    if (started === false) {
+        return;
+    }
+
 
     let btn = this;
 
     userFlash(btn);
 
-    userColor = btn.getAttribute("id");
+
+    let userColor = btn.getAttribute("id");
+
     userSeq.push(userColor);
 
-    checkAns(userSeq.length-1);
 
-    //console.log(userColor);
+    checkAns(userSeq.length - 1);
 
-    //userSeq.push(userColor);
 }
 
+
+// ALL BUTTONS
 let allBtns = document.querySelectorAll(".btn");
 
-for (btn of allBtns) {
+
+for (let btn of allBtns) {
+
     btn.addEventListener("click", btnPress);
+
 }
 
-function reset(){
+
+// RESET
+function reset() {
+
     started = false;
-    gameSeq =[];
+
+    gameSeq = [];
+
     userSeq = [];
-    level =0;
+
+    level = 0;
+
 }
